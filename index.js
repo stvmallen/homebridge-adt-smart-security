5b097a52aff94f06e64b07ff57061f0b7bec272e
Esteban Mallen <esteban.mallen@appdirect.com>
2018-12-14 11:08:24 -0300

    Standardized with single quote

--- index.js
+++ index.js
@@ -9,6 +9,30 @@ const smartSecurityAccessory = function (log, config) {
     this.securityService = new Service.SecuritySystem(this.name);
     this.batteryService = new Service.BatteryService(this.name);
 
+    this.accessoryInfo.setCharacteristic(Characteristic.Manufacturer, "ADT");
+    this.accessoryInfo.setCharacteristic(Characteristic.SerialNumber, "See ADT Smart Security app");
+    this.accessoryInfo.setCharacteristic(Characteristic.Identify, false);
+    this.accessoryInfo.setCharacteristic(Characteristic.Name, this.name);
+
+    this.securityService
+        .getCharacteristic(Characteristic.SecuritySystemCurrentState)
+        .on('get', this.getCurrentState.bind(this))
+        .setProps({validValues: [0, 1, 3, 4]});
+
+    this.securityService
+        .getCharacteristic(Characteristic.SecuritySystemTargetState)
+        .on('set', this.setTargetState.bind(this))
+        .on('get', this.getTargetState.bind(this))
+        .setProps({validValues: [0, 1, 3]});
+
+    this.batteryService
+        .getCharacteristic(Characteristic.BatteryLevel)
+        .on('get', this.getBatteryLevel.bind(this));
+
+    this.batteryService
+        .getCharacteristic(Characteristic.StatusLowBattery)
+        .on('get', this.getLowBatteryStatus.bind(this));
+
     this.adt = new adt(config, this.log);
 
     this.adt.on('state', (state) => {
@@ -18,42 +42,17 @@ const smartSecurityAccessory = function (log, config) {
 
 smartSecurityAccessory.prototype = {
     getServices() {
-        this.log.debug("Getting services");
-
-        this.accessoryInfo.setCharacteristic(Characteristic.Manufacturer, "ADT");
-        this.accessoryInfo.setCharacteristic(Characteristic.SerialNumber, "See ADT Smart Security app");
-        this.accessoryInfo.setCharacteristic(Characteristic.Identify, false);
-        this.accessoryInfo.setCharacteristic(Characteristic.Name, this.name);
-
-        this.securityService
-            .getCharacteristic(Characteristic.SecuritySystemCurrentState)
-            .on("get", this.getCurrentState.bind(this))
-            .setProps({validValues: [0, 1, 3, 4]});
-
-        this.securityService
-            .getCharacteristic(Characteristic.SecuritySystemTargetState)
-            .on("set", this.setTargetState.bind(this))
-            .on("get", this.getTargetState.bind(this))
-            .setProps({validValues: [0, 1, 3]});
-
-        this.batteryService
-            .getCharacteristic(Characteristic.BatteryLevel)
-            .on("get", this.getBatteryLevel.bind(this));
-
-        this.batteryService
-            .getCharacteristic(Characteristic.StatusLowBattery)
-            .on("get", this.getLowBatteryStatus.bind(this));
-
+        this.log.debug('Getting services');
         return [this.accessoryInfo, this.securityService, this.batteryService];
     },
 
     identify(callback) {
-        this.log("Identify requested. Not supported yet.");
+        this.log('Identify requested. Not supported yet.');
         callback();
     },
 
     getBatteryLevel(callback) {
-        this.log("Battery level requested");
+        this.log('Battery level requested');
         this.adt.getState()
             .then((state) => callback(null, state ? state.alarm.batteryLevel : state))
             .catch((error) => {
@@ -63,7 +62,7 @@ smartSecurityAccessory.prototype = {
     },
 
     getLowBatteryStatus(callback) {
-        this.log("Battery status requested");
+        this.log('Battery status requested');
         this.adt.getState()
             .then((state) => callback(null, state ? state.alarm.lowBatteryStatus : state))
             .catch((error) => {
@@ -73,7 +72,7 @@ smartSecurityAccessory.prototype = {
     },
 
     getCurrentState(callback) {
-        this.log("Current state requested");
+        this.log('Current state requested');
         this.adt.getState()
             .then((state) => callback(null, state ? state.alarm.armingState : state))
             .catch((error) => {
@@ -83,7 +82,7 @@ smartSecurityAccessory.prototype = {
     },
 
     getTargetState(callback) {
-        this.log("Target state requested");
+        this.log('Target state requested');
         this.adt.getState()
             .then((state) => callback(null, state ? state.alarm.targetState : state))
             .catch((error) => {
@@ -93,12 +92,12 @@ smartSecurityAccessory.prototype = {
     },
 
     setTargetState(status, callback) {
-        this.log("Received target status", status);
+        this.log('Received target status', status);
         callback(this.adt.setState(status));
     },
 
     updateCharacteristics(status) {
-        this.log.debug("Updating characteristics to", JSON.stringify(status));
+        this.log.debug('Updating characteristics to', JSON.stringify(status));
 
         this.securityService
             .getCharacteristic(Characteristic.SecuritySystemCurrentState)
@@ -122,5 +121,5 @@ module.exports = homebridge => {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
 
-    homebridge.registerAccessory("homebridge-adt-smart-security", "ADT", smartSecurityAccessory);
+    homebridge.registerAccessory('homebridge-adt-smart-security', 'ADT', smartSecurityAccessory);
 };
